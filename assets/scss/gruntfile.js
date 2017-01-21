f97f0b377e06f731df188911edf748c579df6b26
harryberry94 <harryberry94@gmail.com>
2017-01-21 22:56:26 +0000

    Gruntfile.js refinement.

--- assets/scss/gruntfile.js
+++ assets/scss/gruntfile.js
@@ -19,55 +19,20 @@ module.exports = function(grunt) {
 			target: {
 				files: [{
 					expand: true,
-					cwd: 'css',
+					cwd: '../css',
 					src: ['*.css', '!*.min.css'],
 					dest: '../css',
 					ext: '.min.css'
 				}]
 			}
 		},
-		
-		imagemin: {
-			png: {
-				options: {
-					optimizationLevel: 7
-				},
-				files: [{
-					// Set to true to enable the following options…
-					expand: true,
-					// cwd is 'current working directory'
-					cwd: '../images/',
-					src: ['**/*.png'],
-					// Could also match cwd line above. i.e. project-directory/img/
-					dest: '../images/compressed/',
-					ext: '.png'
-				}]
-			},
-			jpg: {
-				options: {
-					progressive: true
-				},
-				files: [{
-					// Set to true to enable the following options…
-					expand: true,
-					// cwd is 'current working directory'
-					cwd: '../images/',
-					src: ['**/*.jpg'],
-					// Could also match cwd. i.e. project-directory/img/
-					dest: '../images/compressed/',
-					ext: '.jpg'
-				}]
-			}
-		},
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass', 'autoprefixer', 'cssmin', 'imagemin']
+				tasks: ['sass', 'autoprefixer', 'cssmin']
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.registerTask('imageminn', ['imagemin']);
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-autoprefixer');
